b83c5e8fee4bf9d9cb76ef0eea9e0c946b0dd439
julian <jnsoretniuq1998@gmail.com>
2023-03-18 04:21:17 -0300

    punto 2f terminado

--- tp1_1.c
+++ tp1_1.c
@@ -2,6 +2,14 @@
 
 int main(void)
 {
-    printf("Hola mundito");
+    int varibale;
+    int *puntero;
+    puntero=&varibale;
+    printf("Hola mundito\n");
+    printf("contenido del puntero %d\n",*puntero);
+    printf("direccion de memoria almacenda en el puntero %p\n", puntero);
+    printf("direccion de memoria de la variable %p\n", &varibale);
+    printf("direccion de memoria del puntero %p\n",&puntero);
+    printf("tamanio de la memoria usado por la variable %d en byte \n",sizeof(varibale));
     return 0;
 }
